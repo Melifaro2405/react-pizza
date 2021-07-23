34f444f412c1b37e9226802f866927ad90ea7fd9
Denis <tomelifaro@yandex.ru>
2021-07-24 00:23:30 +0300

    Добавляем компонент PizzaBlock, настраиваем кнопки

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import {Header} from "./components";
 import {Cart, Home} from "./pages";
 import {Route} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [pizzas, setPizzas] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json')
+      .then(({data}) => setPizzas(data.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header/>
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route path="/cart" component={Cart} />
       </div>
     </div>
